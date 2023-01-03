f08758e73ec5bf515f976df3bb1c9420ba2b0dd6
Crowdbotics <team@crowdbotics.com>
2023-01-03 00:02:43 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "chess_talk_38156",
+  url: "https://chess_talk_38156.botics.co",
+  api: "https://chess_talk_38156.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
